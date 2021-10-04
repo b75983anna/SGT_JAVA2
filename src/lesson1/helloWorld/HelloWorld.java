4728db0edc06f3bb08c4cc2937bbbdc379e6fda5
shegoestech202122 <artjoms.v.petrovs@gmail.com>
2021-10-04 21:06:25 +0300

    Initial commit

---
+++ src/lesson1/helloWorld/HelloWorld.java
@@ -0,0 +1,18 @@
+package lesson1.helloWorld;
+
+import lesson1.helloWorldLibrary.WorldLibrary;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        WorldLibrary theWorld = new WorldLibrary();
+        theWorld.setPlanetName("Earth");
+
+        System.out.println("Welcome to JAVA2 Course!" + WorldLibrary.getName());
+        System.out.println("I live on a planet named " + theWorld.getPlanetName());
+
+        theWorld.setPlanetName("Mars");
+        System.out.println("But now i decided to go to " + theWorld.getPlanetName());
+    }
+}
